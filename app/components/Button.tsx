73707ad4bd9520646170310186b779bfa47df6b8
Adam Shea <avidworks@live.com>
2023-02-12 17:25:41 -0500

    add button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,30 @@
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+
+export interface Props
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonStyles> {}
+
+const buttonStyles = cva("rounded-full px-9 py-4 font-bold border-2 transition-all", {
+	variants: {
+		variant: {
+			primary: "bg-brand text-white border-transparent hover:border-brand hover:bg-transparent",
+			secondary:
+				"bg-transparent text-white border-white hover:border-brand hover:text-brand hover:bg-transparent",
+			danger: "bg-red-500 text-white hover:border-red-500 hover:bg-transparent border-transparent",
+		},
+	},
+	defaultVariants: {
+		variant: "primary",
+	},
+});
+
+function Button({ className, variant, children, ...props }: Props) {
+	return (
+		<button className={buttonStyles({ variant, className })} {...props}>
+			{children}
+		</button>
+	);
+}
+
+export { Button };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/Button";
+import Container from "~/components/Container";
 
 import { spotifyStrategy } from "~/services/auth.server";
 
@@ -12,15 +14,12 @@ export default function Index() {
 	const user = data?.user;
 
 	return (
-		<div style={{ textAlign: "center", padding: 20 }}>
-			<h2>Welcome to Remix!</h2>
-			<p>
-				<a href="https://docs.remix.run">Check out the docs</a> to get started.
-			</p>
+		<Container>
+			<h2 className="text-3xl">Welcome to MusicFinder!</h2>
 			{user ? <p>You are logged in as: {user?.email}</p> : <p>You are not logged in yet!</p>}
 			<Form action={user ? "/logout" : "/auth/spotify"} method="post">
-				<button>{user ? "Logout" : "Log in with Spotify"}</button>
+				<Button className="mt-4">{user ? "Logout" : "Log in with Spotify"}</Button>
 			</Form>
-		</div>
+		</Container>
 	);
 }
